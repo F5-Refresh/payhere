1c5cc799dc496ff86dd23c870decb615187fec13
pekopekopekopayo <dlehddus1285@gmail.com>
2022-07-04 21:29:06 +0900

    #2-feat-core 모델 추가

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
