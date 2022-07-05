6ae6c11ecea39d504bc98b8fc8cd32a6126bbe1a
bmcho <bmcho91@naver.com>
2022-07-06 02:21:11 +0900

    #10-modify-Simple JWT 위한 settings및 model 변경
    settings.py - jwt third party library 추가
    Users model - jwt를 사용하기 위해 변경

--- users/models.py
+++ users/models.py
@@ -1,15 +1,63 @@
 from core.models import TimeStamp as TimeStampModel
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models, transaction
 
 
 # Create your models here.
-class User(TimeStampModel):
-    hashed_password = models.CharField(max_length=255)
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+        )
+
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+            nickname=nickname,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
-    nick_name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=50, unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'nickname',
+    ]
+
+    def __str__(self):
+        return f'{self.nickname}({self.email})'
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
 
     class Meta:
         db_table = 'users'
-
-    def __str__(self):
-        return f'이메일: {self.email} / 닉네임: {self.nick_name}'
