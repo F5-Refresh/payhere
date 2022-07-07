86d366c6e97e69d200cb2b20d4f84b041b9a863c
KIWONY <kien1004@naver.com>
2022-07-07 12:20:41 +0900

    #7-feat-가계부생성,수정,리스트,삭제리스트

--- account_books/serializers.py
+++ account_books/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
-from account_books.models import AccountCategory
 from users.models import User
 
-class AcoountCategorySerializer(serializers.ModelSerializer):
+from account_books.models import AccountBook, AccountCategory
 
+
+class AcoountCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountCategory
         fields = ['id', 'user', 'category_name', 'created_at', 'modified_at']
 
         ordering = ['category_name']
 
+
 class AcoountCategoryPostSerializer(serializers.ModelSerializer):
-    
     def create(self, validated_data):
         user_id = validated_data.get('user').id
         user = User.objects.get(id=user_id)
@@ -21,15 +22,31 @@ class AcoountCategoryPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountCategory
         fields = ['category_name', 'user']
-        
+
+
 class AcoountCategoryPutSerializer(serializers.ModelSerializer):
-    
     def update(self, instance, validated_data):
         instance.category_name = validated_data.get('category_name')
         instance.save()
         return instance
-        
+
     class Meta:
         model = AccountCategory
         fields = ['category_name']
         ordering = ['category_name']
+
+
+# 리스트
+class AccountBookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountBook
+        fields = ['book_name', 'budget', 'delete_flag']
+
+
+# 생성, 수정
+class AccountBookCreatePatchSerializer(serializers.ModelSerializer):
+    nick_name = serializers.StringRelatedField(source="user.nick_name")
+
+    class Meta:
+        model = AccountBook
+        fields = ['user', 'nick_name', 'book_name', 'budget']
--- account_books/urls.py
+++ account_books/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
+from account_books.views import AccountBookList, AccountBookView
 from account_books.views.account_category_views import AcoountCategoryView
 
 urlpatterns = [
     path('account_category', AcoountCategoryView.as_view()),
     path('account_category/<int:account_category_id>', AcoountCategoryView.as_view()),
-    path('account_category/toggle_delete/<int:account_category_id>', AcoountCategoryView.toggle_delete)
+    path('account_category/toggle_delete/<int:account_category_id>', AcoountCategoryView.toggle_delete),
+    path('account-books', AccountBookList.as_view()),
+    path('account-books/<int:book_id>', AccountBookView.as_view()),
+    path('account-books/deleted', AccountBookView.deleted_list),
 ]
+
